865c9450d7c209453d0ef699ad67272b1c860191
Louis Law <louiscklaw@users.noreply.github.com>
2022-08-01 13:44:58 +0800

    update,

--- helloworld-sql-typeorm-pgsql/helloworld/test/users/users.e2e-spec.ts
+++ helloworld-sql-typeorm-pgsql/helloworld/test/users/users.e2e-spec.ts
@@ -20,7 +20,7 @@ describe("Users - /users (e2e)", () => {
       imports: [
         TypeOrmModule.forRoot({
           type: "postgres",
-          host: "localhost",
+          host: "postgres",
           port: 5432,
           username: "postgres",
           password: "postgres",
