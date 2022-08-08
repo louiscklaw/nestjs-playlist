191696e03ad32b6af38fa25e26796e201baef5d8
louiscklaw <louiscklaw@gmail.com>
2022-08-08 16:18:16 +0800

    update,

---
+++ _not_tested/helloworld-file-upload/dist/app.controller.d.ts
@@ -0,0 +1,22 @@
+/// <reference types="multer" />
+import { AppService } from "./app.service";
+import { SampleDto } from "./sample.dto";
+export declare class AppController {
+    private readonly appService;
+    constructor(appService: AppService);
+    sayHello(): {
+        hello: string;
+    };
+    uploadFile(body: SampleDto, file: Express.Multer.File): {
+        body: SampleDto;
+        file: string;
+    };
+    uploadFileAndPassValidation(body: SampleDto, file: Express.Multer.File): {
+        body: SampleDto;
+        file: string;
+    };
+    uploadFileAndFailValidation(body: SampleDto, file: Express.Multer.File): {
+        body: SampleDto;
+        file: string;
+    };
+}
