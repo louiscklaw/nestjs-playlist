499785cc74cee3e2124f3648e674522dd2d3f581
Louis Law <louiscklaw@users.noreply.github.com>
2022-08-09 17:17:22 +0800

    update swagger helloworld,

---
+++ helloworld-sql-typeorm-pgsql/helloworld-swagger-1/helloworld/src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { UsersModule } from "./users/users.module";
+import { TagsModule } from "./tags/tags.module";
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: process.env.HOST_ADDRESS,
+      port: 5432,
+      username: "postgres",
+      password: "postgres",
+      database: "test",
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    UsersModule,
+    TagsModule,
+  ],
+})
+export class AppModule {}
