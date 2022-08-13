b83cf66461ea7b77016374e671fad34cf5732d76
Louis Law <louiscklaw@users.noreply.github.com>
2022-08-13 20:02:45 +0800

    update,

--- helloworld-bull/src/main.ts
+++ helloworld-bull/src/main.ts
@@ -9,33 +9,19 @@ import { AppModule } from './app.module';
 
 const REDIS_HOST = process.env.HOST_ADDRESS || 'localhost';
 
-const connectionOpts = {
-  host: REDIS_HOST,
-  port: 6379,
-};
+const connectionOpts = { host: REDIS_HOST, port: 6379 };
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const serverAdapter = new ExpressAdapter();
-  serverAdapter.setBasePath('/bull-board');
+  serverAdapter.setBasePath('/api/bull-board');
 
-  const aQueue = new Bull('audio', {
-    redis: {
-      ...connectionOpts,
-    },
-  });
+  const aQueue = new Bull('audio', { redis: { ...connectionOpts } });
 
-  createBullBoard({
-    queues: [new BullAdapter(aQueue)],
-    serverAdapter,
-  });
+  createBullBoard({ queues: [new BullAdapter(aQueue)], serverAdapter });
 
-  app.use(
-    '/bull-board',
-
-    serverAdapter.getRouter(),
-  );
+  app.use('/api/bull-board', serverAdapter.getRouter());
 
   await app.listen(3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
