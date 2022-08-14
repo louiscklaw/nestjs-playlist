0047e24caee70dcd15a67940ee4a15fcddfe8f98
louiscklaw <louiscklaw@gmail.com>
2022-08-14 14:34:05 +0800

    update,

--- helloworld-file-upload/src/main.ts
+++ helloworld-file-upload/src/main.ts
@@ -6,8 +6,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  // app.useStaticAssets(join(__dirname, '..', 'uploads'));
-
   await app.listen(3000);
 }
 bootstrap();
