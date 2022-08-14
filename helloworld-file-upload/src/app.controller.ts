8eb71345e25d034a03675f1d1bbcd0ecad08ec29
louiscklaw <louiscklaw@gmail.com>
2022-08-14 14:29:33 +0800

    update,

--- helloworld-file-upload/src/app.controller.ts
+++ helloworld-file-upload/src/app.controller.ts
@@ -9,6 +9,9 @@ import { FileInterceptor } from '@nestjs/platform-express';
 import { AppService } from './app.service';
 
 import * as fs from 'fs';
+import { v4 as uuidv4 } from 'uuid';
+
+const UPLOAD_DIR = './public';
 
 @Controller()
 export class AppController {
@@ -19,13 +22,34 @@ export class AppController {
     return this.appService.getHello();
   }
 
-  @Post('/upload')
+  @Post('upload_stl')
   @UseInterceptors(FileInterceptor('file'))
-  uploadFile(@UploadedFile() file: Express.Multer.File) {
-    console.log(file);
-    fs.writeFile('./uploads/image.png', file.buffer, function (err) {
-      if (err) throw err;
-    });
-    // fs.createWriteStream('./image.png', file.buffer).write();
+  async uploadStl(@UploadedFile() file: Express.Multer.File): Promise<object> {
+    const uuid_filename = uuidv4();
+
+    await fs.writeFileSync(`./public/${uuid_filename}.stl`, file.buffer);
+
+    return {
+      result: 'uploaded',
+      orphan: `${uuid_filename}.stl`,
+      orphan_url: `http://localhost:3000/public/${uuid_filename}.stl`,
+    };
+  }
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadFile(@UploadedFile() file: Express.Multer.File): Promise<object> {
+    const uuid_filename = uuidv4();
+    console.log({ file });
+    // fs.writeFile('./uploads/image.avif', file.buffer, function (err) {
+    //   if (err) throw err;
+    // });
+    await fs.writeFileSync(`${UPLOAD_DIR}/${uuid_filename}.avif`, file.buffer);
+
+    return {
+      result: 'uploaded',
+      orphan: `${uuid_filename}.avif`,
+      orphan_url: `http://localhost:3000/public/${uuid_filename}.avif`,
+    };
   }
 }
