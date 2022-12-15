cdb0716385aefc03e12b52978e8497008014b0d1
louiscklaw <louiscklaw@users.noreply.github.com>
2022-12-16 03:52:33 +0800

    update,

--- truthy-tryout/frontend-helloworld-tryout/cms/src/components/dashboard/overview/overview-private-wallet.js
+++ truthy-tryout/frontend-helloworld-tryout/cms/src/components/dashboard/overview/overview-private-wallet.js
@@ -1,4 +1,4 @@
-import { Avatar, Box, Button, Card, CardActions, Divider, Typography } from '@mui/material';
+import { Avatar, Box, Button, Card, CardActions, Divider, Grid, Typography } from '@mui/material';
 import { alpha, useTheme } from '@mui/material/styles';
 import { ArrowRight as ArrowRightIcon } from '../../../icons/arrow-right';
 import { ChevronDown as ChevronDownIcon } from '../../../icons/chevron-down';
@@ -41,35 +41,32 @@ export const OverviewPrivateWallet = props => {
           padding: '1rem',
         }}
       >
-        <Chart height={160} options={chartOptions} series={chartSeries} type="radialBar" width={160} />
-        <Box
-          sx={{
-            display: 'flex',
-            flexGrow: 1,
-            pt: { sm: 3 },
-            pb: 3,
-            pr: 4,
-            pl: { xs: 4, sm: 0 },
-          }}
-        >
-          <Box sx={{ flexGrow: 1, mr: 3 }}>
-            <Typography color="secondary" variant="h4">
-              999
-            </Typography>
-            <Typography color="textSecondary" sx={{ mt: 1 }} variant="body2">
-              total user
-            </Typography>
-          </Box>
-          <Avatar
-            sx={{
-              backgroundColor: alpha(theme.palette.error.main, 0.08),
-              color: 'error.main',
-            }}
-            variant="rounded"
-          >
-            <ChevronDownIcon fontSize="small" />
-          </Avatar>
-        </Box>
+        <Grid container justifyContent={'center'} alignItems={'center'}>
+          <Grid item xl={4} container justifyContent="center" alignItems="center" sx={{}}>
+            <Chart height={'100%'} options={chartOptions} series={chartSeries} type="radialBar" width={'100%'} />
+          </Grid>
+          <Grid item xl={4} container justifyContent="center" alignItems="center" sx={{}}>
+            <Box>
+              <Typography color="secondary" variant="h4">
+                999
+              </Typography>
+              <Typography color="textSecondary" sx={{ mt: 1 }} variant="body2">
+                総客数
+              </Typography>
+            </Box>
+          </Grid>
+          <Grid item xl={4} container justifyContent="center" alignItems="center" sx={{}}>
+            <Avatar
+              sx={{
+                backgroundColor: alpha(theme.palette.error.main, 0.08),
+                color: 'error.main',
+              }}
+              variant="rounded"
+            >
+              <ChevronDownIcon fontSize="small" />
+            </Avatar>
+          </Grid>
+        </Grid>
       </Box>
       <Divider />
       <CardActions>
